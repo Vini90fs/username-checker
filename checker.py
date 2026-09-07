412124717ecd1678afa2e4bf69a75b3fb2fcf72b
Vinicius Cardoso <vini90fs@gmail.com>
2026-09-07 14:12:43 -0300

    feat: implement username checker

---
+++ checker.py
@@ -0,0 +1,26 @@
+import requests
+from sites import SITES
+
+username = input("Digite o username: ")
+
+for site in SITES:
+    nome = list(site.keys())[0]
+    url = list(site.values())[0]
+
+    url = url.replace("{username}", username)
+
+    resposta = requests.get(url)
+
+    if resposta.status_code == 200:
+        print(nome, "> Encontrado")
+
+    elif resposta.status_code == 403:
+        print(nome, "> Nao foi possivel verificar")
+
+    elif resposta.status_code == 404:
+        print(nome, "> Nao encontrado")
+
+
+
+
+
